c3f8860da212ff3123362fced5a346d9d638061a
Instrutor <felpmel@gmail.com>
2018-06-22 17:37:17 -0300

    Incluir as funções de selecionar e incrementar os itens

--- leaderboard.js
+++ leaderboard.js
@@ -2,11 +2,11 @@
 PlayersList = new Mongo.Collection('players');
 var count=0;
 //Vamos depois modificar pois Bob está sendo todo momento inserido no banco
-PlayersList.insert({
-    name: "Bob"+count,
-    score: 0
-    });
-
+// PlayersList.insert({
+//     name: "Bob"+count,
+//     score: 0
+//     });
+var soma = 0;
 
 //Dividindo a condição para tarefas de cliente
 if (Meteor.isClient) {
@@ -15,11 +15,38 @@ if (Meteor.isClient) {
     //Chamada do Helpers para manipular as "váriaveis" Blaze do html 
     Template.leaderboard.helpers({
         'player': function () {
+            
             return PlayersList.find().fetch();
+            
+        },
+        'classeSelecionado':function(){
+            let selecionado = Session.get("selecionado");   
+            console.log(selecionado);
+            if(this._id==selecionado){                    
+                return "selecionado";
+            }
         }
     });
-}
 
+
+    Template.leaderboard.events({
+        
+        'click .player':function(){
+           
+            Session.set("selecionado",this._id);
+            console.log(this);
+        },
+        'click .increment':function(){
+            soma = soma +5;
+            let selecionado = Session.get("selecionado");
+            console.log(selecionado);
+            PlayersList.update(selecionado,{$inc:{score:5}})
+
+        }
+        
+    });
+
+}
 
 //Dividindo a condição para tarefas de Server
 if (Meteor.isServer) {
