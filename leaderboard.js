c05985a51095bf8291f2f7c8a20cea839e420663
Drinith <felpmel@gmail.com>
2018-06-25 01:40:41 -0300

    Modificação de interface

--- leaderboard.js
+++ leaderboard.js
@@ -5,7 +5,7 @@ PlayersList = new Mongo.Collection('players');
 PlayersList.insert({
     name: "Bob",
     score: 0
-    });
+});
 
 
 //Dividindo a condição para tarefas de cliente
@@ -15,9 +15,41 @@ if (Meteor.isClient) {
     //Chamada do Helpers para manipular as "váriaveis" Blaze do html 
     Template.leaderboard.helpers({
         'player': function () {
-            return PlayersList.find().fetch();
+            return PlayersList.find({},{sort:{score:-1,name:1}});
+        },
+
+        'selecionado': function () {
+
+            var selecionadoSession = Session.get("selecionado");
+            if (this._id == selecionadoSession) {
+
+                return "selected";
+            }
         }
     });
+
+
+    Template.leaderboard.events({
+        // 'click .player': function (event, template) {
+        //     Session.set("selecionado", this._id);
+        // },
+
+        'click .incrementa': function (event, template) {
+
+            //var selecionado = Session.get("selecionado");
+            PlayersList.update(this._id, { $inc: { score: 5 } });
+
+
+        },
+
+        'click .decrementa': function () {
+            //var selecionado = Session.get("selecionado");
+            PlayersList.update(this._id, { $inc: { score: -5 } });
+
+        }
+    });
+
+
 }
 
 
